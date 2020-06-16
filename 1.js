1d3f290bf3cf04c24ba78834dbbac4b3efa5e5fb
Daniel <truongtandat9a3@gmail.com>
2020-06-16 17:16:45 +0700

    Publish fist static website

---
+++ 1.js
@@ -0,0 +1,20 @@
+﻿ $(function(){
+    //resize trang web
+    var docao = $(window).height();
+    $('.top1').css({'height':docao})
+    $(window).resize(function () { 
+        var docao = $(window).height();
+        $('.top1').css({'height':docao})
+    });
+    // hieu ung khi click menu
+    $('.nutmenu').click(function () { 
+        $('.menu').addClass('show');
+        $('.nutmenu').addClass('show');
+        return false;
+    });
+    $('.tat').click(function () { 
+        $('.menu').removeClass('show');
+        $('.nutmenu').removeClass('show');
+        return false;
+    });
+})
